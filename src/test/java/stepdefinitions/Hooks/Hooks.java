15fc31c976665bb58a6633dd97f088bd2f198c82
VIPLOVE BISEN <64732663+viplove29@users.noreply.github.com>
2024-12-17 23:27:04 +0530

    bdd end to end fmf

---
+++ src/test/java/stepdefinitions/Hooks/Hooks.java
@@ -0,0 +1,23 @@
+package stepdefinitions.Hooks;
+
+import io.cucumber.java.After;
+import io.cucumber.java.Before;
+import org.openqa.selenium.WebDriver;
+import utils.DriverFactory;
+
+/** Hooks manage preconditions and cleanup for Cucumber scenarios. */
+public class Hooks {
+
+    @Before
+    public void setUp() {
+        // Can  use Initialize WebDriver
+        System.out.println("Starting a new test scenario.");
+    }
+
+    @After
+    public void tearDown() {
+        // Quit WebDriver
+        DriverFactory.quitDriver();
+        System.out.println("Test scenario completed.");
+    }
+}
