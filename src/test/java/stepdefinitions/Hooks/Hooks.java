ea11d9cab63431009a6f4c2fc3a28910db9e407c
VIPLOVE BISEN <64732663+viplove29@users.noreply.github.com>
2024-12-17 23:30:27 +0530

    spottless apply

--- src/test/java/stepdefinitions/Hooks/Hooks.java
+++ src/test/java/stepdefinitions/Hooks/Hooks.java
@@ -2,22 +2,21 @@ package stepdefinitions.Hooks;
 
 import io.cucumber.java.After;
 import io.cucumber.java.Before;
-import org.openqa.selenium.WebDriver;
 import utils.DriverFactory;
 
 /** Hooks manage preconditions and cleanup for Cucumber scenarios. */
 public class Hooks {
 
-    @Before
-    public void setUp() {
-        // Can  use Initialize WebDriver
-        System.out.println("Starting a new test scenario.");
-    }
+  @Before
+  public void setUp() {
+    // Can  use Initialize WebDriver
+    System.out.println("Starting a new test scenario.");
+  }
 
-    @After
-    public void tearDown() {
-        // Quit WebDriver
-        DriverFactory.quitDriver();
-        System.out.println("Test scenario completed.");
-    }
+  @After
+  public void tearDown() {
+    // Quit WebDriver
+    DriverFactory.quitDriver();
+    System.out.println("Test scenario completed.");
+  }
 }
